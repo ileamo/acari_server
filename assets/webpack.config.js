5189cd7d1627bd4565f0cc8df125a164bc246fc2
Igor Mosunov <imosunov@nsg.ru>
2018-12-25 17:28:24 +0300

    add font awesome

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -31,7 +31,18 @@ module.exports = (env, options) => ({
       {
         test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: '../fonts'
+          }
+        }]
       }
+
     ]
   },
   plugins: [
