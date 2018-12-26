2f3a1365bee0e54d36e04164a199ee558ec5fc91
Igor Mosunov <ileamo@yandex.ru>
2018-12-26 23:24:33 +0300

    put_password_hash

--- lib/acari_server/user_manager/user.ex
+++ lib/acari_server/user_manager/user.ex
@@ -2,11 +2,11 @@ defmodule AcariServer.UserManager.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   schema "users" do
     field :is_admin, :boolean, default: false
     field :password_hash, :string
     field :username, :string
+    field :password, :string, virtual: true
 
     timestamps()
   end
@@ -16,6 +16,19 @@ defmodule AcariServer.UserManager.User do
     user
     |> cast(attrs, [:username, :password_hash, :is_admin])
     |> validate_required([:username, :password_hash, :is_admin])
+    |> validate_length(:username, min: 3, max: 32)
+    |> validate_length(:password, min: 5, max: 32)
     |> unique_constraint(:username)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+
+      _ ->
+        changeset
+    end
   end
 end
