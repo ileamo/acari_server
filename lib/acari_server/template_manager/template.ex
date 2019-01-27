5ba37e628ba514081559dc520c8cddb1512beeec
Igor Mosunov <ileamo@yandex.ru>
2019-01-27 14:54:30 +0300

    Валидация имени шаблона

--- lib/acari_server/template_manager/template.ex
+++ lib/acari_server/template_manager/template.ex
@@ -16,7 +16,20 @@ defmodule AcariServer.TemplateManager.Template do
     template
     |> cast(attrs, [:name, :description, :template, :script_id])
     |> validate_required([:name, :template])
+    |> validate_change(:name, &validate_templ_name/2)
     |> foreign_key_constraint(:script_id)
     |> unique_constraint(:name)
+  end
+
+  defp validate_templ_name(:name, "setup"), do: [name: "Имя шаблона не может быть 'setup'"]
+
+  defp validate_templ_name(:name, name) do
+    case String.match?(name, ~r|^[\w\d\._]+$|) do
+      true ->
+        []
+
+      false ->
+        [name: "Имя шаблона должно состоять из латинских букв, цифр и символов '_', '.'"]
+    end
   end
 end
