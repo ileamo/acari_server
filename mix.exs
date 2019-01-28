c5e6a6b075cc8efe4976be7a32ecf416c39515d4
Igor Mosunov <imosunov@nsg.ru>
2019-01-28 12:14:34 +0300

    Переместить acari, acari_client  в отдельные директории

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule AcariServer.MixProject do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
+  defp elixirc_paths(_), do: ["lib", "acari_client_lib", "acari_lib"]
 
   # Specifies your project dependencies.
   #
