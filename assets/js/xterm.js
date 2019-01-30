b5554a84b4acc29dfc35e33be8b1e0b708c03cda
Igor Mosunov <imosunov@nsg.ru>
2019-01-30 17:15:11 +0300

    Включение терминала  по кнопке

--- assets/js/xterm.js
+++ assets/js/xterm.js
@@ -3,46 +3,54 @@ import {
 } from 'xterm';
 import socket from './socket'
 
-let acari_xterm = document.getElementById('acari-xterm');
-if (acari_xterm) {
-  console.log(window.location.href)
-  console.log(window.location.pathname)
-  let channel = socket.channel("terminal:1", {pathname: window.location.pathname})
-  channel.join()
-  channel.on('output', ({
-    output
-  }) => term.write(output)) // From the Channel
+document.getElementById("start_xterm").addEventListener("click", startXterm, false);
 
-  let term = new Terminal({
-    cols: 80,
-    rows: 25,
-    fontSize: 17,
-    theme: {
-      //background: '#002B36',
-      background: '#073642',
-      foreground: '#eee8d5',
-      black: '#222222',
-      brightBlack: '#454545',
-      red: '#9E5641',
-      brightRed: '#CC896D',
-      green: '#6C7E55',
-      brightGreen: '#C4DF90',
-      yellow: '#CAAF2B',
-      brightYellow: '#FFE080',
-      blue: '#7FB8D8',
-      brightBlue: '#B8DDEA',
-      magenta: '#956D9D',
-      brightMagenta: '#C18FCB',
-      cyan: '#4c8ea1',
-      brightCyan: '#6bc1d0',
-      white: '#808080',
-      brightWhite: '#cdcdcd',
-      cursor: '#eee8d5'
-    }
-  });
+function startXterm() {
+  document.getElementById("start_xterm").remove();
 
-  term.open(acari_xterm);
-  term.on('data', (data) => channel.push('input', {
-    input: data
-  })) // To the Channel
+  let acari_xterm = document.getElementById('acari-xterm');
+  if (acari_xterm) {
+    let channel = socket.channel("terminal:1", {
+      pathname: window.location.pathname
+    })
+    channel.join()
+    channel.on('output', ({
+      output
+    }) => term.write(output)) // From the Channel
+
+    let term = new Terminal({
+      cols: 80,
+      rows: 25,
+      fontSize: 17,
+      theme: {
+        //background: '#002B36',
+        background: '#073642',
+        foreground: '#eee8d5',
+        black: '#222222',
+        brightBlack: '#454545',
+        red: '#9E5641',
+        brightRed: '#CC896D',
+        green: '#6C7E55',
+        brightGreen: '#C4DF90',
+        yellow: '#CAAF2B',
+        brightYellow: '#FFE080',
+        blue: '#7FB8D8',
+        brightBlue: '#B8DDEA',
+        magenta: '#956D9D',
+        brightMagenta: '#C18FCB',
+        cyan: '#4c8ea1',
+        brightCyan: '#6bc1d0',
+        white: '#808080',
+        brightWhite: '#cdcdcd',
+        cursor: '#eee8d5'
+      }
+    });
+
+    term.open(acari_xterm);
+    term.on('data', (data) => channel.push('input', {
+      input: data
+    })) // To the Channel
+  }
 }
+
+//startXterm();
