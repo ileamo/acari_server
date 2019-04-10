6421f39a227736dd5a07bf6ecdedb00c437cce1e
ileamo <imo59y@yandex.ru>
2019-04-10 18:35:52 +0300

    Вычисление узлов, недоступных на всех серверах

--- lib/acari_server_web/views/layout_view.ex
+++ lib/acari_server_web/views/layout_view.ex
@@ -8,6 +8,33 @@ defmodule AcariServerWeb.LayoutView do
   defdelegate num_of_mes(), to: Acari.LinkEventAgent, as: :get_length
 
   def get_mes() do
+    x = get_mes_from_all_nodes()
+
+    bad_nodes_list =
+      case x do
+        [] ->
+          []
+
+        x ->
+          [head | tail] = x |> Enum.map(fn [_, {_, bad_list}] -> bad_list end)
+
+          case head do
+            [] ->
+              []
+
+            _ ->
+              tail
+              |> Enum.reduce_while(MapSet.new(head), fn
+                [], _ -> {:halt, []}
+                bad_list, acc -> {:cont, MapSet.intersection(acc, MapSet.new(bad_list))}
+              end)
+          end
+      end
+
+    {x, bad_nodes_list |> Enum.into([])}
+  end
+
+  defp get_mes_from_all_nodes() do
     [node() | Node.list()]
     |> Enum.sort()
     |> Enum.map(fn node ->
@@ -22,12 +49,12 @@ defmodule AcariServerWeb.LayoutView do
       [
         server,
         mes_list
-        |> Enum.map(fn
-          {tun, link, ts} when is_binary(link) ->
-            {"warning", "#{get_local_time(ts)} #{tun}", "#{link}: нет связи"}
+        |> Enum.map_reduce([], fn
+          {tun, link, ts}, acc when is_binary(link) ->
+            {{"info", "#{get_local_time(ts)} #{tun}", "#{link}: нет связи"}, acc}
 
-          {tun, _, ts} ->
-            {"danger", "#{get_local_time(ts)} #{tun}", "Не доступно"}
+          {tun, _, ts}, acc ->
+            {{"warning", "#{get_local_time(ts)} #{tun}", "Не доступно"}, [tun | acc]}
         end)
       ]
     end)
