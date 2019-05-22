8778105ae706655498ee12b9e9960cf9832f7e66
ileamo <imo59y@yandex.ru>
2019-05-22 13:54:29 +0300

    Опечатка

--- lib/acari_server_web/channels/terminal_channel.ex
+++ lib/acari_server_web/channels/terminal_channel.ex
@@ -7,7 +7,7 @@ defmodule AcariServerWeb.TerminalChannel do
     with [_, tun_name] when is_binary(tun_name) <- Regex.run(~r|/([^/]+)$|, payload["pathname"]),
          node when not is_nil(node) <- AcariServer.Mnesia.get_main_server(tun_name),
          {:ok, terminal} <-
-           Terminal.start_child(Node.self(), %{output_pid: self(), tun_name: tun_name}) do
+           Terminal.start_child(node, %{output_pid: self(), tun_name: tun_name}) do
       Process.link(terminal) |> IO.inspect()
       {:ok, assign(socket, :terminal, terminal)}
     else
