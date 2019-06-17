933c5c6cf8ac735295d848e365d2d0a4bea53aab
ileamo <imo59y@yandex.ru>
2019-06-17 17:02:35 +0300

    Ограничения просмотра туннелей

--- lib/acari_server/user_manager/user_manager.ex
+++ lib/acari_server/user_manager/user_manager.ex
@@ -148,6 +148,17 @@ defmodule AcariServer.UserManager do
     no_auth(conn, "У вас нет прав администратора")
   end
 
+  def is_user_in_group(conn = %{assigns: %{current_user: %{is_admin: true}}}, _opts) do
+    conn
+  end
+
+  def is_user_in_group(
+        %{assigns: %{current_user: user}, params: %{"id" => group_id}} = conn,
+        _opts
+      ) do
+    is_user_in_group(conn, user, [group_id |> String.to_integer()])
+  end
+
   def is_user_in_group(
         %{assigns: %{current_user: user}, params: %{"name" => node_name}} = conn,
         _opts
@@ -157,16 +168,20 @@ defmodule AcariServer.UserManager do
       |> AcariServer.NodeManager.get_node_with_groups()
       |> AcariServer.GroupManager.group_id_list()
 
-    user_groups = user |> Repo.preload(:groups) |> AcariServer.GroupManager.group_id_list()
-
-    case node_groups |> Enum.any?(fn x -> Enum.member?(user_groups, x) end) do
-      true -> conn
-      _ -> no_auth(conn, "Вы не являетесь членом группы")
-    end
+    is_user_in_group(conn, user, node_groups)
   end
 
   def is_user_in_group(conn, _opts) do
     no_auth(conn, "Проблемы с группой")
+  end
+
+  defp is_user_in_group(conn, user, groups_list) do
+    user_groups = user |> Repo.preload(:groups) |> AcariServer.GroupManager.group_id_list()
+
+    case groups_list |> Enum.any?(fn x -> Enum.member?(user_groups, x) end) do
+      true -> conn
+      _ -> no_auth(conn, "Вы не являетесь членом группы")
+    end
   end
 
   defp no_auth(conn, mes) do
--- lib/acari_server_web/controllers/tunnel_controller.ex
+++ lib/acari_server_web/controllers/tunnel_controller.ex
@@ -4,17 +4,46 @@ defmodule AcariServerWeb.TunnelController do
   alias AcariServer.NodeManager
 
   import AcariServer.UserManager, only: [is_user_in_group: 2]
-  plug :is_user_in_group when action in [:show]
+  plug :is_user_in_group when action in [:show, :grp]
 
-
-  def index(conn, _params) do
+  def index(%{assigns: %{current_user: %{is_admin: true}}} = conn, _params) do
     nodes = NodeManager.list_nodes_wo_preload()
     render(conn, "index.html", nodes: nodes, group_id: nil, group_name: "Все")
   end
 
-  def grp(conn, %{"id" => id}) do
+  def index(%{assigns: %{current_user: user}} = conn, _params) do
+    groups =
+      user
+      |> AcariServer.Repo.preload(groups: :nodes)
+      |> Map.get(:groups)
+
+    nodes =
+      groups
+      |> Enum.map(fn %{nodes: nodes} -> nodes end)
+      |> List.flatten()
+      |> Enum.uniq_by(fn %{id: id} -> id end)
+
+    render(conn, "index.html", nodes: nodes, group_id: nil, group_name: "Все", groups: groups)
+  end
+
+  def grp(%{assigns: %{current_user: user = %{is_admin: admin}}} = conn, %{"id" => id}) do
     group = GroupManager.get_group!(id)
-    render(conn, "index.html", nodes: group.nodes, group_id: group.id, group_name: group.name)
+
+    render(conn, "index.html",
+      nodes: group.nodes,
+      group_id: group.id,
+      group_name: group.name,
+      groups:
+        case admin do
+          true ->
+            nil
+
+          _ ->
+            user
+            |> AcariServer.Repo.preload(:groups)
+            |> Map.get(:groups)
+        end
+    )
   end
 
   def show(conn, %{"name" => name}) do
