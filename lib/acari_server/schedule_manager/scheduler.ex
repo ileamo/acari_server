057bc79fbd49745af15f54fe68cba173f48342ea
Igor Mosunov <imosunov@nsg.ru>
2020-01-27 19:02:58 +0300

    update_script_jobs

--- lib/acari_server/schedule_manager/scheduler.ex
+++ lib/acari_server/schedule_manager/scheduler.ex
@@ -24,7 +24,7 @@ defmodule AcariServer.Scheduler.Api do
   def exec_script_on_schedule(shedule_id) do
     with %AcariServer.ScheduleManager.Schedule{} = schedule <-
            AcariServer.ScheduleManager.get_schedule(shedule_id) do
-      IO.inspect(schedule)
+      IO.inspect({NaiveDateTime.utc_now(), schedule.id, schedule.description}, label: "JOB")
     else
       _ -> Logger.error("Scheduler: No Schedule ##{shedule_id}")
     end
@@ -34,5 +34,34 @@ defmodule AcariServer.Scheduler.Api do
     with {:ok, cron_expr} <- Crontab.CronExpression.Parser.parse(schedule.schedule) do
       Scheduler.add_job({cron_expr, {__MODULE__, :exec_script_on_schedule, [schedule.id]}})
     end
+  end
+
+  def update_script_jobs() do
+    job_list =
+      Scheduler.jobs()
+      |> Enum.filter(fn
+        {_, %Quantum.Job{task: {AcariServer.Scheduler.Api, :exec_script_on_schedule, [_]}}} ->
+          true
+
+        _ ->
+          false
+      end)
+      |> Enum.map(fn {ref, %{task: {_, _, [n]}}} -> {ref, n} end)
+
+    job_id_list = job_list |> Enum.map(fn {_, n} -> n end)
+
+    schedule_list = AcariServer.ScheduleManager.list_schedules()
+    schedule_id_list = schedule_list |> Enum.map(fn %{id: n} -> n end)
+
+
+    # Del Jobs
+    job_list
+    |> Enum.reject(fn {_, id} -> Enum.member?(schedule_id_list, id) end)
+    |> Enum.each(fn {ref, _} -> Scheduler.delete_job(ref) end)
+
+    # Add Job
+    schedule_list
+    |> Enum.reject(fn %{id: id} -> Enum.member?(job_id_list, id) end)
+    |> Enum.each(fn sched -> add_job(sched) end)
   end
 end
