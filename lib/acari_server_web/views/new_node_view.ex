e4bc4f1f08dd351eca1086daf1342acefb83150c
imosunov <imosunov@nsg.net.ru>
2020-02-17 23:23:11 +0300

    класс и группы в форме для групповой загрузки клиентов

--- lib/acari_server_web/views/new_node_view.ex
+++ lib/acari_server_web/views/new_node_view.ex
@@ -1,3 +1,5 @@
 defmodule AcariServerWeb.NewNodeView do
   use AcariServerWeb, :view
+  defdelegate group_name_id_pairs_list(), to: AcariServer.GroupManager
+  defdelegate script_name_id_pairs_list(), to: AcariServerWeb.NodeView
 end
