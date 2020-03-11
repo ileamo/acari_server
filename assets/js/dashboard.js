505dc3e23a37d04b82d95a10d3123f3af6088c6b
imosunov <imosunov@nsg.net.ru>
2020-03-11 22:13:22 +0300

    Expanded textarea

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -16,3 +16,17 @@ $(function() {
 $('.popover-dismiss').popover({
   trigger: 'focus'
 })
+
+$(document)
+    .one('focus.autoExpand', 'textarea.autoExpand', function(){
+        var savedValue = this.value;
+        this.value = '';
+        this.baseScrollHeight = this.scrollHeight;
+        this.value = savedValue;
+    })
+    .on('input.autoExpand', 'textarea.autoExpand', function(){
+        var minRows = this.getAttribute('data-min-rows')|0, rows;
+        this.rows = minRows;
+        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 22);
+        this.rows = minRows + rows;
+    });
