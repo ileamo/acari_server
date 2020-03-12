72d6a308829ceddcdd5eedb445b0cff6776ab363
Igor Mosunov <imosunov@nsg.ru>
2020-03-12 19:05:44 +0300

    1.1.14-pre

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AcariServer.MixProject do
   def project do
     [
       app: :acari_server,
-      version: "1.1.13",
+      version: "1.1.14-pre",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
