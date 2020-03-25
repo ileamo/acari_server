02bc7077eea87985f4fc67f8e71b7c95076d05df
imosunov <imosunov@nsg.net.ru>
2020-03-25 18:06:16 +0300

    Валидатор для elexir

--- lib/validator/validator.ex
+++ lib/validator/validator.ex
@@ -2,6 +2,7 @@ defmodule AcariServer.Validator do
   def validators() do
     %{
       "nsgconfig" => &AcariServer.Validator.NSGconfParser.parse/1,
+      "elixir" => &exs_validator/1,
       "<NO_VALIDATOR>" => fn _ -> :ok end
     }
   end
@@ -9,5 +10,12 @@ defmodule AcariServer.Validator do
   def get_validator_list() do
     validators() |> Enum.map(fn {name, _} -> name end)
     |> Enum.sort()
+  end
+
+  defp exs_validator(text) do
+    case Code.string_to_quoted(text) do
+      {:ok, _} -> :ok
+      {:error, {line, error, _token}} -> "#{line}: #{error}"
+    end
   end
 end
