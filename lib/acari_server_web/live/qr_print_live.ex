0680f063b8677fb53611baa784e088428ead37a1
imosunov <imosunov@nsg.net.ru>
2020-06-20 01:19:55 +0300

    вычисление размера QR

--- lib/acari_server_web/live/qr_print_live.ex
+++ lib/acari_server_web/live/qr_print_live.ex
@@ -2,40 +2,61 @@ defmodule AcariServerWeb.QRPrintLive do
   use AcariServerWeb, :live_view
 
   @impl true
-  def mount(params, %{"clients_list" => ids} = session, socket) do
-    IO.inspect(params: params, session: session)
+  def mount(_params, %{"clients_list" => ids} = _session, socket) do
+    url =
+      AcariServer.SysConfigManager.get_conf_by_key("system.url.mobile") ||
+        "http://localhost"
 
     qr_list =
       String.split(ids, ",")
-      |> IO.inspect()
+      |> Enum.filter(fn x ->
+        case Integer.parse(x) do
+          {_, ""} -> true
+          _ -> false
+        end
+      end)
+      |> Enum.map(fn id -> AcariServer.NodeManager.get_node_hash(id) end)
+      |> Enum.reject(&is_nil/1)
+      |> Enum.map(fn %{hash: hash, name: name, description: description, address: address} ->
+        %{
+          qr_svg: (url <> "/client/" <> hash) |> EQRCode.encode() |> EQRCode.svg(width: 200),
+          name: name,
+          description: description,
+          address: address
+        }
+      end)
+
+    socket =
+      assign(socket,
+        qr_list: qr_list,
+        qr_num: length(qr_list),
+        top: "15",
+        bottom: "15",
+        left: "15",
+        right: "15",
+        gap: "1",
+        cols: "3",
+        rows: "8"
+      )
+
+    ass = socket.assigns
 
     {:ok,
      assign(socket,
-       qr_list: qr_list,
-       qr_num: length(qr_list),
-       top: "15",
-       bottom: "15",
-       left: "15",
-       right: "15",
-       gap: "1",
-       cols: "3",
-       rows: "8"
+       qr_for_page: int(ass.cols) * int(ass.rows),
+       svg_size: get_svg_size(ass)
      )}
   end
 
   @impl true
   def handle_event("draw", params, socket) do
-    IO.inspect(params: params)
+    socket = validate(socket, params)
+    ass = socket.assigns
 
     {:noreply,
      assign(socket,
-       top: params["top"],
-       bottom: params["bottom"],
-       left: params["left"],
-       right: params["right"],
-       gap: params["gap"],
-       cols: params["cols"],
-       rows: params["rows"]
+       qr_for_page: int(ass.cols) * int(ass.rows),
+       svg_size: get_svg_size(ass)
      )}
   end
 
@@ -44,12 +65,56 @@ defmodule AcariServerWeb.QRPrintLive do
     {:noreply, socket}
   end
 
-  defp get_gap(gap) do
-    IO.inspect(gap)
+  defp get_svg_size(ass) do
+    min(
+      (297 - float(ass.top) - float(ass.bottom)) / int(ass.rows) - 5,
+      (210 - float(ass.left) - float(ass.right)) / int(ass.cols) - 2
+    )
+  end
 
-    case Float.parse(gap) do
-      {n, _} -> n / 2
-      _ -> 0.5
+  defp validate(socket, params) do
+    ass = socket.assigns
+
+    params = [
+      float(ass, :top, params["top"]),
+      float(ass, :bottom, params["bottom"]),
+      float(ass, :left, params["left"]),
+      float(ass, :right, params["right"]),
+      float(ass, :gap, params["gap"]),
+      int(ass, :cols, params["cols"]),
+      int(ass, :rows, params["rows"])
+    ]
+
+    assign(socket, params)
+  end
+
+  defp int(val) do
+    case Integer.parse(String.trim(val)) do
+      {n, ""} -> n
+      _ -> 0
     end
+  end
+
+  defp float(val) do
+    case Float.parse(String.trim(val)) do
+      {n, ""} -> n
+      _ -> 0
+    end
+  end
+
+  defp int(ass, tag, val) do
+    {tag,
+     case Integer.parse(String.trim(val)) do
+       {n, ""} -> to_string(n)
+       _ -> ass[tag] <> " "
+     end}
+  end
+
+  defp float(ass, tag, val) do
+    {tag,
+     case Float.parse(String.trim(val)) do
+       {n, ""} -> to_string(n)
+       _ -> ass[tag] <> " "
+     end}
   end
 end
