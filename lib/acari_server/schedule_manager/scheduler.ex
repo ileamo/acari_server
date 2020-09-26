1b8a3ef0b75a9c93cdbe5170e918546794ff0ddb
imosunov <imosunov@nsg.net.ru>
2020-09-26 16:06:26 +0300

    spawn quantum function

--- lib/acari_server/schedule_manager/scheduler.ex
+++ lib/acari_server/schedule_manager/scheduler.ex
@@ -180,4 +180,13 @@ defmodule AcariServer.Scheduler.Api do
     |> Enum.reject(fn %{id: id} -> Enum.member?(job_id_list, id) end)
     |> Enum.each(fn sched -> add_job(sched) end)
   end
+
+  def quantum_apply(func, params) do
+    with pid when is_pid(pid) <- :global.whereis_name({Highlander, AcariServer.Scheduler}),
+         node when is_atom(node) <- :erlang.node(pid) do
+      Node.spawn(node, AcariServer.Scheduler.Api, func, params)
+    else
+      _ -> Logger.error("Can't config sneduler")
+    end
+  end
 end
