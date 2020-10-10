01262bb2cfe2e8ad134f8750a63334ed220938dd
imosunov <imosunov@nsg.net.ru>
2020-10-10 17:02:59 +0300

    user config: if api hide admin and groups

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,12 +1,33 @@
 let user_form_is_admin = document.getElementById("user-form-is-admin")
 if (user_form_is_admin) {
   user_form_is_admin.addEventListener("change", userIsAdmin, false);
+
   function userIsAdmin() {
     let checked = this.checked
     if (checked) {
       document.getElementById("user-form-groups").hidden = true
-    } else{
+    } else {
       document.getElementById("user-form-groups").hidden = false
+    }
+  }
+}
+
+let user_form_api = document.getElementById("user-form-api")
+if (user_form_api) {
+  user_form_api.addEventListener("change", userApi, false);
+
+  function userApi() {
+    let checked = this.checked
+    if (checked) {
+      document.getElementById("user-form-groups").hidden = true
+      document.getElementById("user-form-admin").hidden = true
+    } else {
+      document.getElementById("user-form-admin").hidden = false
+      if (user_form_is_admin.checked) {
+        document.getElementById("user-form-groups").hidden = true
+      } else {
+        document.getElementById("user-form-groups").hidden = false
+      }
     }
   }
 }
