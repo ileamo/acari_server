b1fef7ceb1ebfd9b98f6b567fae71318cd30917a
imosunov <imosunov@nsg.net.ru>
2021-08-28 13:02:57 +0300

    fix docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 chown docker /var/log
-setcap cap_net_admin=ep /opt/app/erts-10.7.1/bin/beam.smp \
+setcap cap_net_admin=ep /opt/app/erts-11.2/bin/beam.smp \
   cap_net_admin=ep /sbin/ip
 #su - docker -c "screen -d -m -h 4096 /bin/bash"
 exec su-exec docker /opt/app/bin/bogatka_docker $@
+
+
