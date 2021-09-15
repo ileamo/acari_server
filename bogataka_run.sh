830ce8248ee68d9b10e8772957d7fdc7dadd110c
imosunov <imosunov@nsg.net.ru>
2021-09-15 18:16:15 +0300

    Добавление статического IP для интерфейса в контейнере

--- bogataka_run.sh
+++ bogataka_run.sh
@@ -1,15 +1,15 @@
   docker run -it --rm\
     --name ubootovo \
     --hostname ubootovo \
+    --net bogatka \
     -e DB_HOST=bogatka-db \
-    -e DB_HOSTS_RW="bogatka-db:" \
-    -e DB_HOSTS_RO="bogatka-db" \
+    -e DB_HOSTS_RW="bogatka-db-master" \
+    -e DB_HOSTS_RO="bogatka-db-master" \
     -p 443:50443 \
     -p 50019:50019 \
     -v /etc/ssl/bogatka:/etc/ssl/bogatka:ro \
     -v /var/log/bogatka_ubootovo:/var/log \
     -v /etc/localtime:/etc/localtime:ro \
-    --link bogatka-db \
     --cap-add=NET_ADMIN \
     --device /dev/net/tun:/dev/net/tun \
     bogatka
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+if [[ ! -z "$ETH0_STATIC_IP" ]]; then
+  ip address add dev eth0 $ETH0_STATIC_IP
+fi
 chown docker /var/log
 setcap cap_net_admin=ep /opt/app/erts-11.2/bin/beam.smp \
   cap_net_admin=ep /sbin/ip
