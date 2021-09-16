402673a489aa03c8cdc57cdb2f0aa5045696fa58
imosunov <imosunov@nsg.net.ru>
2021-09-16 14:06:47 +0300

    Назначение номеров портов через переменные среды

--- config/rt_docker.exs
+++ config/rt_docker.exs
@@ -30,11 +30,6 @@ config :acari_server, AcariServer.RepoManager,
 
 config :acari_server, AcariServer, allow_unconfigured: false
 
-config :acari_server, AcariServer.Listener,
-  port: 50019,
-  keyfile: "/etc/ssl/bogatka/ssl.key",
-  certfile: "/etc/ssl/bogatka/ssl.crt"
-
 config :acari_server, :zabbix,
   zbx_ext_url: System.get_env("ZBX_EXT_URL"),
   zbx_web_port: System.get_env("ZBX_WEB_PORT"),
@@ -50,3 +45,50 @@ config :acari_server, :openstreetmap, provider_url: System.get_env("MAP_PROVIDER
 config :acari_server, AcariServerWeb.Endpoint,
   secret_key_base: AcariServer.Application.random_string(64),
   live_view: [signing_salt: AcariServer.Application.random_string(32)]
+
+bg_http_port = AcariServer.system_get_integer_env("BG_HTTP_PORT") || 50080
+bg_https_port = AcariServer.system_get_integer_env("BG_HTTPS_PORT") || 50443
+bg_api_port = AcariServer.system_get_integer_env("BG_API_PORT") || 50444
+bg_tls_port = AcariServer.system_get_integer_env("BG_TLS_PORT") || 50019
+bg_tcp_port = AcariServer.system_get_integer_env("BG_TCP_PORT") || 50020
+
+config :acari_server, AcariServerWeb.Endpoint,
+  http: [port: bg_http_port],
+  https: [
+    port: bg_https_port,
+    keyfile: "/etc/ssl/bogatka/ssl.key",
+    # cacertfile: "/etc/ssl/acari/chain.pem",
+    certfile: "/etc/ssl/bogatka/ssl.crt",
+    cipher_suite: :strong
+    # versions: [:"tlsv1.2", :"tlsv1.1", :tlsv1, :sslv3]
+  ],
+
+  # This is critical for ensuring web-sockets properly authorize.
+  url: [host: "localhost", port: bg_http_port],
+  check_origin: false,
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: ".",
+  # debug_errors: true,
+  version: Application.spec(:acari_server, :vsn)
+
+config :acari_server, AcariServerWeb.EndpointPub,
+  https: [
+    port: bg_api_port,
+    keyfile: "/etc/ssl/bogatka/ssl.key",
+    # cacertfile: "/etc/ssl/acari/chain.pem",
+    certfile: "/etc/ssl/bogatka/ssl.crt",
+    cipher_suite: :strong
+    # versions: [:"tlsv1.2", :"tlsv1.1", :tlsv1, :sslv3]
+  ],
+  server: true
+
+# Do not print debug messages in production
+config :logger, level: :info
+
+config :acari_server, AcariServer.Listener,
+  port: bg_tls_port,
+  keyfile: "/etc/ssl/bogatka/ssl.key",
+  certfile: "/etc/ssl/bogatka/ssl.crt"
+
+config :acari_server, AcariServer.TCPListener, port: bg_tcp_port
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@ if [[ ! -z "$ETH0_STATIC_IP" ]]; then
   ip address add dev eth0 $ETH0_STATIC_IP
 fi
 chown docker /var/log
-setcap cap_net_admin=ep /opt/app/erts-11.2/bin/beam.smp \
-  cap_net_admin=ep /sbin/ip
+setcap cap_net_admin,cap_net_bind_service=+ep /opt/app/erts-11.2/bin/beam.smp \
+  cap_net_admin=+ep /sbin/ip
 #su - docker -c "screen -d -m -h 4096 /bin/bash"
 exec su-exec docker /opt/app/bin/bogatka_docker $@
 
